6b72e09b60f512abedc00cc99f41d76c574f23e3
Cooper Wallace <CooperWallace@users.noreply.github.com>
2018-07-04 12:27:59 -0600

    Update Polybar launch script
    Add support for multiple monitors
    Add std redirection to avoid terminal output

--- .config/i3/scripts/launchpoly.sh
+++ .config/i3/scripts/launchpoly.sh
@@ -6,7 +6,16 @@ killall -q polybar
 # Wait until the processes have been shut down
 while pgrep -x polybar >/dev/null; do sleep 1; done
 
-# Launch bar1 and bar2
-polybar mybar &
+
+if type "xrandr"; then
+  for m in $(xrandr --query | grep " connected" | cut -d" " -f1); do
+    MONITOR=$m polybar --reload mybar > /dev/null 2>&1 &
+  done
+else
+  polybar --reload mybar > /dev/null 2>&1 &
+fi
+
+# Launch bar for Laptop screen
+#MONITOR=eDP-1 polybar mybar &
 
 echo "Bars launched..."
